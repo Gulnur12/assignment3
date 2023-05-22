99c002921b6fc511e3031f9785fec641575d9b47
Gulnur12 <106649358+Gulnur12@users.noreply.github.com>
2023-05-22 22:10:09 +0600

    Update main.go

--- gulnur/services/contact/cmd/app/main.go
+++ gulnur/services/contact/cmd/app/main.go
@@ -6,14 +6,12 @@ import (
 )
 
 func main() {
-    // Параметры подключения к БД
     host := "localhost"
     port := "5432"
     user := "postgres"
     password := "toha2004"
     dbname := "example"
 
-    // Подключение к БД PostgreSQL
     db, err := postgres.ConnectDB(host, port, user, password, dbname)
     if err != nil {
         fmt.Println("Failed to connect to the database:", err)
@@ -23,6 +21,4 @@ func main() {
     }
 
     defer db.Close()
-
-    // Ваши дальнейшие действия с БД...
 }
